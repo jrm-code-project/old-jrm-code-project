c5a9e2f5c71e4d7a69fbb659074a9f80cd8d2bc6
eval.apply <eval.apply@c747a578-372c-0410-91be-492e479bcfcf>
2007-06-11 16:59:58 +0000

    Add package file.

---
+++ emacs/slime-2.0j/package.lisp
@@ -0,0 +1,40 @@
+;;; This file contains the package definitions for SLIME/SWANK
+;;; It is better to keep package definition files separate so you
+;;; can configure the package topology without loading and executing code.
+
+;;; Author: jrm
+
+(in-package "CL-USER")
+
+(defpackage "MONITOR" (:nicknames "MON") (:use "COMMON-LISP")
+  (:export
+   "*MONITORED-FUNCTIONS*"
+   "DISPLAY-MONITORING-RESULTS"
+   "MONITOR"
+   "MONITOR-ALL"
+   "MONITOR-FORM"
+   "MONITORED"
+   "MONITORING-ENCAPSULATE"
+   "MONITORING-UNENCAPSULATE"
+   "REPORT"
+   "REPORT-MONITORING"
+   "RESET-ALL-MONITORING"
+   "RESET-MONITORING-INFO"
+   "UNMONITOR"
+   "WITH-MONITORING"
+   ))
+
+(defpackage "SWANK" (:use "COMMON-LISP"
+                          "CLOS"
+                          #+clisp "GRAY"
+                          #+lispworks "STREAM"
+                          )
+  #+clisp (:import-from "CLOS"
+                        "SLOT-DEFINITION-DOCUMENTATION"))
+
+(defpackage "SWANK-IO-PACKAGE"
+    (:use)
+  (:import-from "COMMON-LISP"
+                "NIL"
+                "T"
+                "QUOTE"))
