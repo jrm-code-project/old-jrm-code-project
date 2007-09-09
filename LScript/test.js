083255cb675e0d46948ca1d30c4d6e63c7521d6d
eval.apply <eval.apply@c747a578-372c-0410-91be-492e479bcfcf>
2007-09-09 01:53:07 +0000

    First checkin for active scripting experiments.

---
+++ LScript/test.js
@@ -0,0 +1,6 @@
+WScript.Echo ("This is test.js");
+WScript.Echo ("Attempting to create object");
+var obj = WScript.CreateObject ("LScript.LScript");
+WScript.Echo ("obj is" + obj);
+WScript.Echo ("obj is" + obj1);
+WScript.Echo ("test.js exits");
