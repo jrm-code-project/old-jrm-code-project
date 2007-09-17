3995ff179c9d1293a43c5f06681a4b4942184be1
eval.apply <eval.apply@c747a578-372c-0410-91be-492e479bcfcf>
2007-09-17 17:54:45 +0000

    Add Package and SimpleEvaluator.

---
+++ trunk/Jrm.Lisp/Package.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lisp
+{
+    public class Package
+    {
+        private static List<Package> AllPackages = new List<Package> ();
+        private static Package CommonLispUserPackage = new Package("COMMON-LISP-USER",
+            new string [] {"CL-USER"});
+
+        private string name;
+        private List<string> nicknames;
+
+        public Package (string name, string [] nicknames)
+        {
+            this.name = String.Intern(name);
+            this.nicknames = new List<string> ();
+            if (nicknames != null)
+                foreach (string nickname in nicknames)
+                    this.nicknames.Add (nickname);
+            AllPackages.Add (this);
+        }
+
+        public override string ToString ()
+        {
+            return "#<PACKAGE \"" + this.name + "\">";
+        }
+
+        static public Package CommonLispUser
+        {
+            get
+            {
+                return CommonLispUserPackage;
+            }
+        }
+
+    }
+}
