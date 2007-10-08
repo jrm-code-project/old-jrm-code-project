ba9eba87de160572e0f8f6a5c780ac3df65295b9
eval.apply <eval.apply@c747a578-372c-0410-91be-492e479bcfcf>
2007-10-08 18:02:29 +0000

    Fixes, refactoring, etc.

--- Jrm.Lisp/Function.cs
+++ Jrm.Lisp/Function.cs
@@ -6,4 +6,9 @@ using System.Text;
 namespace Lisp
 {
     public delegate object Applicable (params object [] arguments);
+
+    public delegate object Function2 (object arg0, object arg1);
+    public delegate object Function3 (object arg0, object arg1, object arg2);
+
+    public delegate bool Predicate (object argument);
 }
