f2e32a64d211bc30539c868b73256d4c41f7ae5d
eval.apply <eval.apply@c747a578-372c-0410-91be-492e479bcfcf>
2008-04-10 20:03:04 +0000

    Added spectral.scm

--- Jrm.Lisp/Cons.cs
+++ Jrm.Lisp/Cons.cs
@@ -8,7 +8,7 @@ namespace Lisp
     [CLSCompliant(true)]
     public struct ConsEnumerator : IEnumerator
     {
-        Cons head;
+        readonly Cons head;
         Cons current;
         bool exhausted;
 
@@ -17,6 +17,35 @@ namespace Lisp
             this.head = head;
             this.current = null;
             this.exhausted = false;
+        }
+
+        public override bool Equals (object obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType () != typeof (ConsEnumerator))
+                return false;
+
+            ConsEnumerator that = (ConsEnumerator) obj;
+            return this.head == that.head
+                && this.current == that.current
+                && this.exhausted == that.exhausted;
+        }
+
+        public override int GetHashCode ()
+        {
+            return this.head.GetHashCode ();
+        }
+
+        public static bool operator== (ConsEnumerator left, ConsEnumerator right)
+        {
+            return left.head == right.head
+                && left.current == right.current
+                && left.exhausted == right.exhausted;
+        }
+
+        public static bool operator!= (ConsEnumerator left, ConsEnumerator right)
+        {
+            return !(left == right);
         }
 
         public bool MoveNext ()
@@ -50,7 +79,7 @@ namespace Lisp
     }
 
     [CLSCompliant(true)]
-    public sealed class Cons : ICollection, IEnumerable, IList
+    public sealed class Cons : ReadOnlyCollectionBase, ICollection, IEnumerable, IList
     {
         [DebuggerBrowsable (DebuggerBrowsableState.Never)]
         readonly object car;
@@ -84,16 +113,26 @@ namespace Lisp
 
         public void CopyTo (Array array)
         {
+            Utility.Ignore (this);
+            Utility.Ignore(array);
             throw new NotImplementedException ("CopyTo");
         }
 
         public void CopyTo (Array array, int arrayIndex)
         {
+            Utility.Ignore (this);
+            Utility.Ignore(array);
+            Utility.Ignore(arrayIndex);
             throw new NotImplementedException ("CopyTo");
         }
 
         public void CopyTo (int index, Array array, int arrayIndex, int count)
         {
+            Utility.Ignore (this);
+            Utility.Ignore(index);
+            Utility.Ignore(array);
+            Utility.Ignore(arrayIndex);
+            Utility.Ignore(count);
             throw new NotImplementedException ("CopyTo");
         }
 
@@ -102,7 +141,8 @@ namespace Lisp
         {
             get
             {
-                return false;
+                Utility.Ignore (this);
+                throw new NotImplementedException("IsSynchronized");
             }
         }
 
@@ -210,6 +250,8 @@ namespace Lisp
 
         void ICollection.CopyTo (Array array, int index)
         {
+            Utility.Ignore(array);
+            Utility.Ignore(index);
             throw new NotImplementedException ();
         }
         [DebuggerBrowsable (DebuggerBrowsableState.Never)]
@@ -265,16 +307,46 @@ namespace Lisp
 
     public struct ConsListEnumerator<T> : IEnumerator<T>
     {
-        ConsList<T> head;
-        ConsList<T> current;
+        readonly ConsCollection<T> head;
+        ConsCollection<T> current;
         bool exhausted;
 
-        public ConsListEnumerator (ConsList<T> head)
+        public ConsListEnumerator (ConsCollection<T> head)
         {
             this.head = head;
             this.current = null;
             this.exhausted = false;
         }
+
+        public override bool Equals (object obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType () != typeof (ConsListEnumerator<T>))
+                return false;
+
+            ConsListEnumerator<T> that = (ConsListEnumerator<T>) obj;
+            return this.head == that.head
+                && this.current == that.current
+                && this.exhausted == that.exhausted;
+        }
+
+        public override int GetHashCode ()
+        {
+            return this.head.GetHashCode ();
+        }
+
+        public static bool operator == (ConsListEnumerator<T> left, ConsListEnumerator<T> right)
+        {
+            return left.head == right.head
+                && left.current == right.current
+                && left.exhausted == right.exhausted;
+        }
+
+        public static bool operator != (ConsListEnumerator<T> left, ConsListEnumerator<T> right)
+        {
+            return !(left == right);
+        }
+
 
         public void Dispose () {
         }
@@ -319,18 +391,29 @@ namespace Lisp
     }
 
     // A linked list of cons cells.
-    public sealed class ConsList<T> : ICollection<T>, ICollection, IEnumerable<T>, IEnumerable
+    public sealed class ConsCollection<T> : IList<T>, IList, ICollection<T>, ICollection, IEnumerable<T>, IEnumerable
     {
         [DebuggerBrowsable (DebuggerBrowsableState.Never)]
         readonly T car;
 
         [DebuggerBrowsable (DebuggerBrowsableState.Never)]
-        readonly ConsList<T> cdr;
+        readonly ConsCollection<T> cdr;
 
-        public ConsList (T car, ConsList<T> cdr)
+        public ConsCollection (T car, ConsCollection<T> cdr)
         {
             this.car = car;
             this.cdr = cdr;
+        }
+
+        public ConsCollection (T [] vector, int start, int limit)
+        {
+            if (vector == null) throw new ArgumentNullException ("vector");
+            if (start >= limit) throw new NotImplementedException ("bogus arguments");
+            this.car = vector [start];
+            if (start + 1 == limit)
+                this.cdr = null;
+            else
+                this.cdr = new ConsCollection<T> (vector, start + 1, limit);
         }
 
         public T Car
@@ -342,7 +425,7 @@ namespace Lisp
             }
         }
 
-        public ConsList<T> Cdr
+        public ConsCollection<T> Cdr
         {
             [DebuggerStepThrough]
             get
@@ -351,27 +434,27 @@ namespace Lisp
             }
         }
 
-        public static ConsList<T> SubvectorToList (T [] vector, int start, int limit)
-        {
-            if (vector == null) {
-                if (start == limit)
-                    return null;
-                throw new ArgumentNullException ("vector");
-            }
+        //public static ConsCollection<T> SubvectorToList (T [] vector, int start, int limit)
+        //{
+        //    if (vector == null) {
+        //        if (start == limit)
+        //            return null;
+        //        throw new ArgumentNullException ("vector");
+        //    }
 
-            ConsList<T> answer = null;
-            int count = 1;
-            for (int i = start; i < limit; i++) {
-                answer = new ConsList<T> (vector [limit - count], answer);
-                count += 1;
-            }
-            return answer;
-        }
+        //    ConsCollection<T> answer = null;
+        //    int count = 1;
+        //    for (int i = start; i < limit; i++) {
+        //        answer = new ConsCollection<T> (vector [limit - count], answer);
+        //        count += 1;
+        //    }
+        //    return answer;
+        //}
 
-        public static ConsList<T> VectorToList (T [] vector)
-        {
-            return SubvectorToList (vector, 0, vector.Length);
-        }
+        //public static ConsCollection<T> VectorToList (T [] vector)
+        //{
+        //    return SubvectorToList (vector, 0, vector.Length);
+        //}
 
 
         void ICollection<T>.Add (T thing)
@@ -391,18 +474,23 @@ namespace Lisp
                      && ((ICollection<T>) this.Cdr).Contains (thing));
         }
 
-        void ICollection<T>.CopyTo (T[] array, int arrayIndex)
+        void ICollection.CopyTo (Array array, int index)
         {
+            Utility.Ignore(array);
+	    Utility.Ignore(index);
             throw new NotImplementedException ("CopyTo");
         }
 
-        void ICollection.CopyTo (Array array, int arrayIndex)
+        void ICollection<T>.CopyTo (T[] array, int index)
         {
+            Utility.Ignore(array);
+	    Utility.Ignore(index);
             throw new NotImplementedException ("CopyTo");
         }
 
         bool ICollection<T>.Remove (T thing)
         {
+            Utility.Ignore(thing);
             throw new NotSupportedException ();
         }
 
@@ -412,9 +500,9 @@ namespace Lisp
             get
             {
                 int length = 1;
-                ConsList<T> tail = this;
+                ConsCollection<T> tail = this;
                 while (true) {
-                    ConsList<T> next = tail.cdr;
+                    ConsCollection<T> next = tail.cdr;
                     if (next == null) {
                         if (tail.cdr == null)
                             break;
@@ -433,9 +521,9 @@ namespace Lisp
             get
             {
                 int length = 1;
-                ConsList<T> tail = this;
+                ConsCollection<T> tail = this;
                 while (true) {
-                    ConsList<T> next = tail.cdr;
+                    ConsCollection<T> next = tail.cdr;
                     if (next == null) {
                         if (tail.cdr == null)
                             break;
@@ -490,5 +578,118 @@ namespace Lisp
                 return this.car;
             }
         }
+
+        #region IList<T> Members
+
+        int IList<T>.IndexOf (T item)
+        {
+            throw new NotImplementedException ();
+        }
+
+        void IList<T>.Insert (int index, T item)
+        {
+            throw new NotImplementedException ();
+        }
+
+        void IList<T>.RemoveAt (int index)
+        {
+            throw new NotImplementedException ();
+        }
+
+        T IList<T>.this [int index]
+        {
+            get
+            {
+                throw new NotImplementedException ();
+            }
+            set
+            {
+                throw new NotImplementedException ();
+            }
+        }
+
+        #endregion
+
+        #region IList Members
+
+        int IList.Add (object value)
+        {
+            throw new NotImplementedException ();
+        }
+
+        void IList.Clear ()
+        {
+            throw new NotImplementedException ();
+        }
+
+        bool IList.Contains (object value)
+        {
+            throw new NotImplementedException ();
+        }
+
+        int IList.IndexOf (object value)
+        {
+            throw new NotImplementedException ();
+        }
+
+        void IList.Insert (int index, object value)
+        {
+            throw new NotImplementedException ();
+        }
+
+        [DebuggerBrowsable (DebuggerBrowsableState.Never)]
+
+        bool IList.IsFixedSize
+        {
+            get { return true; }
+        }
+
+        [DebuggerBrowsable (DebuggerBrowsableState.Never)]
+
+        bool IList.IsReadOnly
+        {
+            get { return true; }
+        }
+
+        void IList.Remove (object value)
+        {
+            throw new NotImplementedException ();
+        }
+
+        void IList.RemoveAt (int index)
+        {
+            throw new NotImplementedException ();
+        }
+
+        object IList.this [int index]
+        {
+            get
+            {
+                throw new NotImplementedException ();
+            }
+            set
+            {
+                throw new NotImplementedException ();
+            }
+        }
+
+        #endregion
+
+        #region ICollection Members
+
+        [DebuggerBrowsable (DebuggerBrowsableState.Never)]
+
+        bool ICollection.IsSynchronized
+        {
+            get { throw new NotImplementedException (); }
+        }
+        [DebuggerBrowsable (DebuggerBrowsableState.Never)]
+
+        object ICollection.SyncRoot
+        {
+            get { throw new NotImplementedException (); }
+        }
+
+        #endregion
     }
 }
