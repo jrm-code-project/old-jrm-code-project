23c13f82961e6b6d5e52f524751fb039bacee89d
eval.apply <eval.apply@c747a578-372c-0410-91be-492e479bcfcf>
2008-05-07 16:37:23 +0000

    Switch directory.

--- MIT-Scheme/Listener/Listener.cs
+++ MIT-Scheme/Listener/Listener.cs
@@ -10,7 +10,8 @@ namespace Listener
         {
             string originalDirectory = System.Environment.CurrentDirectory;
             try {
-                System.Environment.CurrentDirectory = System.Environment.CurrentDirectory + "\\..\\..\\..\\Runtime\\";
+                // System.Environment.CurrentDirectory = System.Environment.CurrentDirectory + "\\..\\..\\..\\Runtime7\\";
+                System.Environment.CurrentDirectory = "C:\\Documents and Settings\\jmarshall\\test\\mit-scheme\\v7\\src\\runtime\\";
                 SCode bootstrap = Fasl.Fasload ("make.bin") as SCode;
                 Interpreter interpreter = new Interpreter ();
                 Termination term = interpreter.Start (bootstrap);
