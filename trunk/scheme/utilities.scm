e82798fe8fd913f812b845741dc4cab5ab1a3b64
eval.apply <eval.apply@c747a578-372c-0410-91be-492e479bcfcf>
2008-05-08 19:42:51 +0000

    Fix log2, 10log10 and untabify.

--- trunk/scheme/utilities.scm
+++ trunk/scheme/utilities.scm
@@ -9,16 +9,16 @@
 
   (define (head-reduce-loop state element tail)
     (if (pair? tail)
-	(head-reduce-loop (procedure state element) (car tail) (cdr tail))
-	(begin (if (not (null? tail))
-		   (head-reduce-error))
-	       (procedure state element))))
+        (head-reduce-loop (procedure state element) (car tail) (cdr tail))
+        (begin (if (not (null? tail))
+                   (head-reduce-error))
+               (procedure state element))))
 
   (if (pair? list)
       (head-reduce-loop init (car list) (cdr list))
       (begin (if (not (null? list))
-		 (head-reduce-error))
-	     init)))
+                 (head-reduce-error))
+             init)))
 
 (define (adjoin/eq list element)
   (if (memq element list)
@@ -78,43 +78,56 @@
 (define (difference/eq left right)
   (declare (integrate left))
   (head-reduce (lambda (result left-element)
-		 (if (memq left-element right)
-		     result
-		     (adjoin/eq result left-element)))
-	       '()
-	       left))
+                 (if (memq left-element right)
+                     result
+                     (adjoin/eq result left-element)))
+               '()
+               left))
 
 (declare (integrate-operator difference/eqv))
 (define (difference/eqv left right)
   (declare (integrate left))
   (head-reduce (lambda (result left-element)
-		 (if (memv left-element right)
-		     result
-		     (adjoin/eqv result left-element)))
-	       '()
-	       left))
+                 (if (memv left-element right)
+                     result
+                     (adjoin/eqv result left-element)))
+               '()
+               left))
 
 (declare (integrate-operator difference/equal))
 (define (difference/equal left right)
   (declare (integrate left))
   (head-reduce (lambda (result left-element)
-		 (if (member left-element right)
-		     result
-		     (adjoin/eq result left-element)))
-	       '()
-	       left))
+                 (if (member left-element right)
+                     result
+                     (adjoin/eq result left-element)))
+               '()
+               left))
 
 (declare (integrate-operator is-nan?))
 (define (is-nan? x)
   (not (or (flo:negative? x)
-	   (flo:positive? x)
-	   (flo:zero? x))))
+           (flo:positive? x)
+           (flo:zero? x))))
+
+(define-integrable (flo:10log10 x)
+  (flo:* 4.3429448190325175 (flo:log x)))
 
 (define-integrable (10log10 x)
-  (flo:* 10.0 (flo:/ (log x) (log 10.0))))
+  (flo:10log10 (exact->inexact x)))
 
-(define-integrable (log2 x)
-  (flo:/ (log x) (log 2.0)))
+(define-integrable (flo:log2 x)
+  (flo:* 1.4426950408889634 (flo:log x)))
+
+(define (log2 x)
+  (cond ((inexact? x) (flo:log2 x))
+        ((not (integer? x)) (flo:log2 (exact->inexact x)))
+        (else (do ((x1 x (/ x1 2))
+                   (a  0 (+ a 1)))
+                  ((not (even? x1))
+                   (if (= x1 1)
+                       a
+                       (+ a (flo:log2 (exact->inexact x1)))))))))
 
 (declare (integrate-operator probability->entropy))
 (define (probability->entropy/bits p)
@@ -133,45 +146,53 @@
 
 (define-integrable (sum weight list)
   (head-reduce (lambda (total item)
-		 (declare (integrate total item))
-		 (+ total (weight item)))
-	       0
-	       list))
+                 (declare (integrate total item))
+                 (+ total (weight item)))
+               0
+               list))
 
 (define-integrable (count-if predicate list)
   (head-reduce (lambda (total element)
-		 (declare (integrate element))
-		 (if (predicate element)
-		     (fix:+ total 1)
-		     total))
-	       0
-	       list))
+                 (declare (integrate element))
+                 (if (predicate element)
+                     (fix:+ total 1)
+                     total))
+               0
+               list))
 
 (define-integrable (count-if-not predicate list)
   (head-reduce (lambda (total element)
-		 (declare (integrate element))
-		 (if (predicate element)
-		     total
-		     (fix:+ total 1)))
-	       0
-	       list))
+                 (declare (integrate element))
+                 (if (predicate element)
+                     total
+                     (fix:+ total 1)))
+               0
+               list))
 
 (define-integrable (split-list predicate input receiver)
   (define (iter tail yes no)
     (cond ((pair? tail) (let ((this-element (car tail)))
-			  (if (predicate this-element)
-			      (iter (cdr tail) (cons this-element yes) no)
-			      (iter (cdr tail) yes (cons this-element no)))))
-	  ((null? tail) (receiver yes no))
-	  (else (error "Bad input list"))))
+                          (if (predicate this-element)
+                              (iter (cdr tail) (cons this-element yes) no)
+                              (iter (cdr tail) yes (cons this-element no)))))
+          ((null? tail) (receiver yes no))
+          (else (error "Bad input list"))))
   (iter input '() '()))
 
 (define (collect-if predicate)
   (lambda (list)
     (head-reduce (lambda (answer item)
-		   (if (predicate item)
-		       (cons item answer)
-		       answer))
-		 '()
-		 list)))
+                   (if (predicate item)
+                       (cons item answer)
+                       answer))
+                 '()
+                 list)))
 
+(define (collect-if-not predicate)
+  (lambda (list)
+    (head-reduce (lambda (answer item)
+                   (if (predicate item)
+                       answer
+                       (cons item answer)))
+                 '()
+                 list)))
