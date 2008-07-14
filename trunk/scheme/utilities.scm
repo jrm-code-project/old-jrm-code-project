f8f5ce3b034197804516741813a863622157518a
eval.apply <eval.apply@c747a578-372c-0410-91be-492e479bcfcf>
2008-07-14 17:43:08 +0000

    Error messages.  Better version of gamma.  Newton's method.

--- trunk/scheme/utilities.scm
+++ trunk/scheme/utilities.scm
@@ -1,23 +1,37 @@
 (declare (usual-integrations))
 
+(define (error:not-positive object #!optional caller)
+  (error:wrong-type-argument object "positive number" (if (eq? caller #!default) #f caller)))
+
+(define (error:not-nonnegative object #!optional caller)
+  (error:wrong-type-argument object "non-negative number" (if (eq? caller #!default) #f caller)))
+
+(define (guarantee-positive object #!optional caller)
+  (if (not (positive? object))
+      (error:not-positive object caller)))
+
+(define (guarantee-nonnegative object #!optional caller)
+  (if (negative? object)
+      (error:not-nonnegative object caller)))
+
 (declare (integrate-operator head-reduce))
 (define (head-reduce procedure init list)
   (declare (integrate procedure init))
 
-  (define (head-reduce-error)
-    (error:wrong-type-argument list "list" 'HEAD-REDUCE))
+  (define-integrable (head-reduce-error irritant)
+    (error:not-list irritant 'HEAD-REDUCE))
 
   (define (head-reduce-loop state element tail)
     (if (pair? tail)
         (head-reduce-loop (procedure state element) (car tail) (cdr tail))
         (begin (if (not (null? tail))
-                   (head-reduce-error))
+                   (head-reduce-error tail))
                (procedure state element))))
 
   (if (pair? list)
       (head-reduce-loop init (car list) (cdr list))
       (begin (if (not (null? list))
-                 (head-reduce-error))
+                 (head-reduce-error tail))
              init)))
 
 (define (adjoin/eq list element)
@@ -115,7 +129,7 @@
 			      (iter (cdr tail) (adjoin/eq intersection item) difference)
 			      (iter (cdr tail) intersection (adjoin/eq difference item)))))
 	  ((null? tail) (receiver intersection difference))
-	  (else (error "Improper list" left))))
+	  (else (error:not-list left interdifference/eq))))
   (iter left '() '()))
 
 (declare (integrate-operator is-nan?))
@@ -124,11 +138,23 @@
            (flo:positive? x)
            (flo:zero? x))))
 
+(define-integrable (log->10log10 x)
+  (flo:* 4.3429448190325175 x))
+
+(define-integrable (10log10->log x)
+  (flo:/ x 4.3429448190325175))
+
 (define-integrable (flo:10log10 x)
-  (flo:* 4.3429448190325175 (flo:log x)))
+  (log->10log10 (flo:log x)))
 
 (define-integrable (10log10 x)
   (flo:10log10 (exact->inexact x)))
+
+(define-integrable (flo:inverse-10log10 x)
+  (flo:exp (10log10->log x)))
+
+(define-integrable (inverse-10log10 x)
+  (flo:inverse-10log10 (exact->inexact x)))
 
 (define-integrable (flo:log2 x)
   (flo:* 1.4426950408889634 (flo:log x)))
@@ -143,28 +169,75 @@
                        a
                        (+ a (flo:log2 (exact->inexact x1)))))))))
 
-(define (int:gamma x)
+(define-integrable *pi* 3.141592653589793)
+(define-integrable *log-pi* (flo:log *pi*))
+
+(define *lanczos-coefficients*
+  '(
+    0.99999999999999709182
+    57.156235665862923517
+    -59.597960355475491248
+    14.136097974741747174
+    -0.49191381609762019978
+    .33994649984811888699e-4
+    .46523628927048575665e-4
+    -.98374475304879564677e-4
+    .15808870322491248884e-3
+    -.21026444172410488319e-3
+    .21743961811521264320e-3
+    -.16431810653676389022e-3
+    .84418223983852743293e-4
+    -.26190838401581408670e-4
+    .36899182659531622704e-5
+    ))
+
+(define (flo:log-gamma xx)
+  (if (flo:positive? xx)
+      (if (flo:< xx 1.0)
+	  ;; Reflection
+	  (let ((one-minus-x (flo:- 1.0 xx)))
+	    (flo:- (flo:+ *log-pi* (flo:log one-minus-x))
+		   (flo:+ (flo:log-gamma (flo:+ 1.0 one-minus-x))
+			  (flo:log (flo:sin (flo:* *pi* one-minus-x))))))
+	  ;; Lanczos approximation
+
+	  (let* ((g (/ 607.0 128.0))
+		 (sum 0.0))
+	    (do ((i (- (length *lanczos-coefficients*) 1) (- i 1)))
+		((= i 0))
+	      (set! sum (+ sum (/ (list-ref *lanczos-coefficients* i) (+ xx i)))))
+	    (set! sum (+ sum (car *lanczos-coefficients*)))
+	    (let ((tmp (+ xx g .5)))
+	      (+ (* .5 (log (* *pi* 2)))
+		 (log (/ sum xx))
+		 (- tmp)
+		 (* (+ xx .5) (log tmp))))))
+
+	  (error:not-positive xx 'log-gamma)))
+
+(define (log-gamma xx)
+  (flo:log-gamma (exact->inexact xx)))
+
+(define (10log10-gamma x)
+  (log->10log10 (log-gamma x)))
+
+(define (int:factorial x)
   (define (iter x answer)
-    (if (fix:< x 11)
+    (if (fix:< x 2)
 	answer
 	(iter (fix:- x 1) (int:* answer x))))
+  (if (int:negative? x)
+      (error:not-nonnegative x 'int:factorial)
+      (iter x 1)))
 
-  (cond	((not (int:positive? x))
-	 (error "int:gamma defined only for positive integers."))
-	((int:= x 1) 1)
-	((int:= x 2) 1)
-	((int:= x 3) 2)
-	((int:= x 4) 6)
-	((int:= x 5) 24)
-	((int:= x 6) 120)
-	((int:= x 7) 720)
-	((int:= x 8) 5040)
-	((int:= x 9) 40320)
-	((int:= x 10) 362880)
-	(else (iter x 3628800))))
-
-(define-integrable (factorial x)
-  (gamma (+ x 1)))
+(define (int:gamma x)
+  (define (iter x answer)
+    (if (fix:< x 2)
+	answer
+	(iter (fix:- x 1) (int:* answer x))))
+  (if (int:negative? (- x 1))
+      (error:not-positive x 'int:gamma)
+      (iter (- x 1) 1)))
 
 (define (double-factorial x)
   (define (iter x answer)
@@ -174,14 +247,37 @@
   (iter x 1))
 
 (define (gamma x)
-  (cond ((integer? x) (int:gamma x))
-	((integer? (- x 1/2)) (let ((n (- x 1/2)))
-				(if (zero? n)
+  (cond ((integer? x) (int:gamma (inexact->exact x)))
+	((integer? (- x 1/2)) (let ((n (* 2 (- x 1))))
+				(if (= n -3/2)
 				    (inexact->exact 1.772453850905516027298167483341)
 				    (* (inexact->exact 1.772453850905516027298167483341)
-				       (/ (double-factorial (- (* n 2) 1))
-					  (expt 2 n))))))
-	(else (error "Can't take gamma of this: " x))))
+				       (/ (double-factorial n)
+					  (expt 2 (/ (+ n 1) 2)))))))
+	(else (exp (log-gamma x)))))
+
+(define (10log10-gamma-ratio num den)
+  ;; Compute log (gamma(num)/gamma(den))
+  (flo:- (10log10-gamma num) (10log10-gamma den)))
+
+(define (10log10-bernoulli-beta alpha beta m N)
+  (guarantee-positive alpha '10log10-bernoulli-beta)
+  (guarantee-positive beta '10log10-bernoulli-beta)
+  (guarantee-exact-nonnegative-integer m '10log10-bernoulli-beta)
+  (guarantee-exact-positive-integer N '10log10-bernoulli-beta)
+  (flo:+ (10log10-gamma-ratio (+ alpha m) alpha)
+	 (flo:+ (10log10-gamma-ratio (+ beta (int:- N m)) beta)
+		(10log10-gamma-ratio (+ alpha beta) (+ (+ alpha beta) N)))))
+
+;; Compute the log (x + y) given log(x) and log(y)
+;; avoiding the problem of overflow.
+(define-integrable (flo:log-sum left right)
+  (if (flo:> right left)
+      (flo:+ right (flo:10log10 (flo:+ 1.0 (flo:inverse-10log10 (flo:- left right)))))
+      (flo:+ left (flo:10log10 (flo:+ 1.0 (flo:inverse-10log10 (flo:- right left)))))))
+
+(define (log-sum left right)
+  (flo:log-sum (exact->inexact left) (exact->inexact right)))
 
 (declare (integrate-operator probability->entropy/bits))
 (define (probability->entropy/bits p)
@@ -219,6 +315,15 @@
                0
                list))
 
+(declare (integrate-operator flo:sum))
+(define (flo:sum weight list)
+  (declare (integrate list))
+  (head-reduce (lambda (total item)
+                 (declare (integrate total item))
+                 (flo:+ total (weight item)))
+               0.0
+               list))
+
 (define-integrable (count-if predicate list)
   (head-reduce (lambda (total element)
                  (declare (integrate element))
@@ -244,7 +349,7 @@
                               (iter (cdr tail) (cons this-element yes) no)
                               (iter (cdr tail) yes (cons this-element no)))))
           ((null? tail) (receiver yes no))
-          (else (error "Bad input list"))))
+          (else (error:not-list tail 'divide-list))))
   (iter input '() '()))
 
 (define (collect-if predicate)
@@ -264,3 +369,33 @@
                        (cons item answer)))
                  '()
                  list)))
+
+(define (shuffle-list list)
+  (map cdr (sort (map (lambda (element) (cons (random 1.0) element)) list) (lambda (l r) (< (car l) (car r))))))
+
+(define (newtons-method f guess dx)
+
+  (define (fprime x)
+    (/ (- (f (+ x dx)) (f (- x dx)))
+       (* dx 2)))
+
+  (define (iter guess)
+    (let ((fx (f guess)))
+      (if (< (abs fx) dx)
+	  guess
+	  (iter (- guess (/ fx (fprime guess)))))))
+
+  (iter guess))
+
+;; Similar to Newton's method,
+;; returns x such that (f x) is within dx of zero.
+(define (secant-method f guess0 guess1 dx)
+  (define (iter guess last-guess last-answer)
+    (let ((fx (f guess)))
+      (if (< (abs fx) dx)
+	  guess
+	  (iter (- guess (* fx (/ (- guess last-guess)
+				  (- fx last-answer)))) 
+		guess
+		fx))))
+  (iter guess1 guess0 (f guess0)))
