f438d50fcb05640b85ad00a9195013bba201b176
eval.apply <eval.apply@c747a578-372c-0410-91be-492e479bcfcf>
2009-09-08 02:53:25 +0000

    Forgot a couple of files.

---
+++ trunk/MIT-Scheme/Microcode/PartialResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microcode
+{
+    public struct PartialResult
+    {
+        readonly SCode residual;
+
+        public PartialResult (SCode residual)
+        {
+            this.residual = residual;
+        }
+
+        public SCode Residual
+        {
+            get
+            {
+                return this.residual;
+            }
+        }
+    }
+}
